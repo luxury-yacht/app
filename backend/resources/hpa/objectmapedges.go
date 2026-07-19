5b90962044a51515e36e5165330b16b2724dfd80
John Jeffers <john@jeffers.cc>
2026-07-18 23:08:57 -0600

    wip

--- backend/resources/hpa/objectmapedges.go
+++ backend/resources/hpa/objectmapedges.go
@@ -2,7 +2,6 @@ package hpa
 
 import (
 	"github.com/luxury-yacht/app/backend/kind/objectmapspec"
-	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	autoscalingv2 "k8s.io/api/autoscaling/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -11,8 +10,6 @@ import (
 func ObjectMapEdges(clusterID string, obj metav1.Object) []objectmapspec.Edge {
 	var edge objectmapspec.Edge
 	switch hpa := obj.(type) {
-	case *autoscalingv1.HorizontalPodAutoscaler:
-		edge = objectmapspec.Edge{Type: objectmapspec.EdgeScales, Link: BuildV1Facts(clusterID, hpa).ScaleTarget}
 	case *autoscalingv2.HorizontalPodAutoscaler:
 		edge = objectmapspec.Edge{Type: objectmapspec.EdgeScales, Link: BuildFacts(clusterID, hpa).ScaleTarget}
 	default:
